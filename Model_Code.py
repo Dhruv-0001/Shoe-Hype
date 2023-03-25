2b4b652daacb27962ef014010a4ae83ce1c42b89
Dhruv Tyagi <109892892+Dhruv-0001@users.noreply.github.com>
2023-03-25 09:15:34 +0530

    Optimized the code

--- Model_Code.py
+++ Model_Code.py
@@ -11,6 +11,29 @@ from numpy.linalg import norm
 import os
 import pickle
 from tqdm import tqdm
+from sklearn.neighbors import NearestNeighbors
+import cv2
+import pandas as pd
+import csv
+
+
+#*********CREATING MODEL & EXTRACTING FEATURES OF UPLOADED IMAGE***********
+
+model = ResNet50 (weights='imagenet', include_top=False, input_shape=(224, 224,3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
+
+img = image.load_img("sample images/shoes2",target_size=(224, 224))
+img_array = image.img_to_array(img)
+expanded_img_array = np.expand_dims(img_array, axis=0)
+preprocessed_img = preprocess_input(expanded_img_array)
+result = model.predict(preprocessed_img).flatten()
+normalized_result = result / norm(result)
+
 
 #******EXTRACTING FEATURES FROM OUR IMAGES AND STORING THE RESULTS******
 
@@ -30,45 +53,11 @@ pickle.dump(filenames_shoes, open('filenames_shoes.pkl', 'wb'))
 
 #****LOADING EXTRACTED FEATURES************
 
-import tensorflow
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.layers import GlobalMaxPooling2D
-from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
-import numpy as np
-from numpy.linalg import norm
-import os
-import pickle
-from tqdm import tqdm
-from sklearn.neighbors import NearestNeighbors
-import cv2
-
 feature_list_shoes = np.array(pickle.load(open('embeddings_shoes.pkl','rb')))
 filenames_shoes = pickle.load(open('filenames_shoes.pkl','rb'))
 
 
-#*********EXTRACTING FEATURES OF UPLOADED IMAGE***********
-
-model = ResNet50 (weights='imagenet', include_top=False, input_shape=(224, 224,3))
-model.trainable = False
-
-model = tensorflow.keras.Sequential([
-    model,
-    GlobalMaxPooling2D()
-])
-
-img = image.load_img("sample images/shoes2",target_size=(224, 224))
-img_array = image.img_to_array(img)
-expanded_img_array = np.expand_dims(img_array, axis=0)
-preprocessed_img = preprocess_input(expanded_img_array)
-result = model.predict(preprocessed_img).flatten()
-normalized_result = result / norm(result)
-
-
 #******FINDING NEAREST VECTORS FOR RECOMMENDATION*********
-
-import pandas as pd
-import os
-import csv
 
 product = pd.read_csv("dataset_csv.csv")
 data = os.listdir("SHOES_IMAGES")
